85795e791412cc2cb390d87b6f70f30561330a16
Zinadin Zidan <zidan.roking@gmail.com>
2022-02-20 17:50:23 +0300

    merlinx: Use pe recovery as default recovery
    Change-Id: Ia41cd5f05c1e2de5875111fdcaabc93ac9556a74

--- aosp_merlinx.mk
+++ aosp_merlinx.mk
@@ -29,6 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from our custom product configuration
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_AOSP_RECOVERY := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 #
